188a3edbb645a16c714a55a1591170c4e8008567
John Joseph <jpothen8@gmail.com>
2024-10-10 13:49:07 -0400

    catch skugg sound effects

--- scripts/catch_skugg.gd
+++ scripts/catch_skugg.gd
@@ -4,10 +4,15 @@ var timer = 1.0
 
 var skugg = preload("res://scenes/skugg_running.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var playing_lebron = false
+var playing_goat = false
+var playing_bing = false
 
+var player_done = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:	
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -15,12 +20,42 @@ func _process(delta: float) -> void:
 	if timer > 0.0:
 		$RichTextLabel.text = "Skugg is happy now! He wants to play with you in "+ str(round(timer)) + " seconds! Try to tickle him!"
 	else:
-		$RichTextLabel.text = ""
-		if Global.skugg_off_screen:
+		if player_done:
+			$RichTextLabel.text = "Oh my Skibbity! You got Skugg."
+		elif timer < -50.0:
+			$RichTextLabel.text = ""
+			$bingchilling.stop()
+		elif timer < -40.0:
+			$RichTextLabel.text ="'The surest way to win is to not lose' - Sun Tzu, The Art of War"
+			if not playing_bing:
+				$bingchilling.play()
+				playing_bing = true
+		elif timer < -35.0:
+			$RichTextLabel.text = ""
+			$lebron.stop()
+		elif timer < -30.0:
+			$RichTextLabel.text = "If the 2020 Pistons beat LeBron James, you could definitely beat this."
+			if not playing_lebron:
+				$lebron.play()
+				playing_lebron = true
+		elif timer < -27.5:
+			$RichTextLabel.text = ""
+			$goat.stop()
+		elif timer < -20.0:
+			$RichTextLabel.text = "This is why Skugg is the GOAT."
+			if not playing_goat:
+				$goat.play()
+				playing_goat = true
+		else:
+			$RichTextLabel.text = ""
+		
+		if Global.skugg_off_screen and not player_done:
 			var instance = skugg.instantiate()
 			instance.visible = true
 			add_child(instance)
 			Global.skugg_off_screen = false
 		if Global.skugg_pressed:
 			Global.skugg_pressed = false
+			player_done = true
 			print("LIGMA")
+		
