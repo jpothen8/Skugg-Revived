a248b60f2067b5f418352a332913a01ee54bcb65
John Joseph <jpothen8@gmail.com>
2024-11-20 12:15:29 -0500

    speech to text

--- scenes/dvd.gd
+++ scenes/dvd.gd
@@ -3,18 +3,23 @@ extends Node2D
 var fullsizeX = DisplayServer.screen_get_size().x
 var fullsizeY = DisplayServer.screen_get_size().y
 
-var velocity = Vector2i(20, 20)
+var velocity = Vector2i(7.5, 7.5)
+
+var effect: AudioEffectCapture
+var recording
+
+var idx : int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	get_window().unresizable = true
-	get_window().borderless = true
+	get_window().borderless = true 
 	
 	get_window().size = Vector2i(fullsizeX * 0.35, fullsizeY*0.35)
-	print(fullsizeX)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
 	get_window().position += velocity
 	
 	if (get_window().position.y < 0
@@ -24,3 +29,5 @@ func _process(delta: float) -> void:
 	if (get_window().position.x < 0
 		or get_window().position.x + get_window().size.x > fullsizeX):
 		velocity = Vector2i(-velocity.x, velocity.y)
+		
+		
