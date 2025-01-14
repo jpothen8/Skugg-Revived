1dc1445f70d304f36cd891f40c33378005dc8199
John Joseph <jpothen8@gmail.com>
2025-01-14 12:13:35 -0500

    add a BOSS

--- scripts/world.gd
+++ scripts/world.gd
@@ -2,9 +2,11 @@ extends Node2D
 
 var NewRunningEnemy
 var NewEnemy
+var NewBoss
 var random = RandomNumberGenerator.new()
 var newEnemy = load("res://scenes/enemy.tscn")
 var newRunningEnemy = load("res://scenes/running_enemy.tscn")
+var newBoss = load("res://scenes/boss.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	SpawnEnemy()
@@ -27,16 +29,21 @@ func _process(delta: float) -> void:
 func SpawnEnemy():
 	print("RUNNING NEW ENEMY")
 	#Global.enemyCount = Global.enemies 
-	for i in Global.enemies:
-		var randomize = random.randi_range(1,2)
-		if(randomize == 1):
-			NewEnemy = newEnemy.instantiate()
-			NewEnemy.position = Vector2(random.randi_range(50,200),random.randi_range(50,200))
-			get_parent().add_child(NewEnemy)
-		else:
-			NewRunningEnemy = newRunningEnemy.instantiate()
-			NewRunningEnemy.position = Vector2(random.randi_range(50,200),random.randi_range(50,200))
-			get_parent().add_child(NewRunningEnemy)
+	if ((Global.enemies - 2) % 4 == 0):
+		NewBoss = newBoss.instantiate()
+		NewBoss.position = Vector2(random.randi_range(50,200),random.randi_range(50,200))
+		get_parent().add_child(NewBoss)
+	else:
+		for i in Global.enemies:
+			var randomize = random.randi_range(1,2)
+			if(randomize == 1):
+				NewEnemy = newEnemy.instantiate()
+				NewEnemy.position = Vector2(random.randi_range(50,200),random.randi_range(50,200))
+				get_parent().add_child(NewEnemy)
+			else:
+				NewRunningEnemy = newRunningEnemy.instantiate()
+				NewRunningEnemy.position = Vector2(random.randi_range(50,200),random.randi_range(50,200))
+				get_parent().add_child(NewRunningEnemy)
 	
 
 
